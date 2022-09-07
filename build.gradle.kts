eeb494528bd394a3f54aa18fb51155b0753d8ec0
Thomas McKernan <tmeaglei@gmail.com>
2022-09-06 21:00:53 -0700

    to intel

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
