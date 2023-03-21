76de88587c18beb498aa37a940739d1f3ee7708d
Thomas McKernan <tmeaglei@gmail.com>
2023-03-20 18:37:25 -0700

    log4j setup and adding per request logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,26 +21,36 @@ repositories {
     maven(uri("https://mvn.mckernant1.com/release"))
 }
 
+configurations.implementation {
+    exclude("org.springframework.boot", "spring-boot-starter-logging")
+}
+
 dependencies {
+    // Spring
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-
+    // Utils
     implementation("com.github.mckernant1.lol:esports-api:0.0.22")
     implementation("com.github.mckernant1:kotlin-utils:0.0.34")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 
-
+    // Logging
     implementation("org.slf4j:slf4j-api:2.0.5")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
 
+    // AWS
     implementation(platform("software.amazon.awssdk:bom:2.18.25"))
     implementation("software.amazon.awssdk:dynamodb-enhanced")
 
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/kotlin/com/mckernant1/lol/esports/api/filters/RequestLoggingFilter.kt
@@ -0,0 +1,38 @@
+package com.mckernant1.lol.esports.api.filters
+
+import com.github.mckernant1.standalone.measureDuration
+import jakarta.servlet.FilterChain
+import jakarta.servlet.annotation.WebFilter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.MDC
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.util.UUID
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@WebFilter(filterName = "RequestLoggingFilter", urlPatterns = ["/*"])
+class RequestLoggingFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = UUID.randomUUID().toString()
+        MDC.put("RequestId", token)
+
+        logger.info("START")
+
+        val timeTaken = measureDuration {
+            filterChain.doFilter(request, response)
+        }
+
+        logger.info("RequestURI: '${request.requestURI}' Method: '${request.method}' ResponseStatus: '${response.status}' RequestDuration: '${timeTaken.toMillis()}ms'")
+        logger.info("END")
+
+        MDC.remove("RequestId")
+    }
+}
