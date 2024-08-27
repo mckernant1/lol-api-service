7233fcf0b4de7510cd4eed2ffe40b764067acb54
Thomas McKernan <tmeaglei@gmail.com>
2024-08-26 20:11:43 -0500

    add enable scheduling

--- src/main/kotlin/com/mckernant1/lol/esports/api/Runner.kt
+++ src/main/kotlin/com/mckernant1/lol/esports/api/Runner.kt
@@ -2,8 +2,10 @@ package com.mckernant1.lol.esports.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class Runner
 
 fun main(args: Array<String>) {
