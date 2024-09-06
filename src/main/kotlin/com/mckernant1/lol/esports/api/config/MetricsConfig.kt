e0da80936a01dd71902b968c787704c47c63c747
Thomas McKernan <tmeaglei@gmail.com>
2024-09-06 18:40:23 -0500

    remove host default dimension

--- src/main/kotlin/com/mckernant1/lol/esports/api/config/MetricsConfig.kt
+++ src/main/kotlin/com/mckernant1/lol/esports/api/config/MetricsConfig.kt
@@ -1,15 +1,11 @@
 package com.mckernant1.lol.esports.api.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.mckernant1.commons.metrics.Dimension
 import com.mckernant1.commons.metrics.Metrics
 import com.mckernant1.commons.metrics.impls.CloudWatchMetrics
-import com.mckernant1.commons.metrics.impls.NoopMetrics
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient
-import java.net.InetAddress
 
 @Configuration
 class MetricsConfig {
@@ -19,9 +15,7 @@ class MetricsConfig {
     }
 
     @Bean
-    fun defaultDimensions(): Set<Dimension> = setOf(
-        Dimension("Host", "${ProcessHandle.current().pid()}@${InetAddress.getLocalHost().hostName}")
-    )
+    fun defaultDimensions(): Set<Dimension> = setOf()
 
     @Bean
     fun metrics(
