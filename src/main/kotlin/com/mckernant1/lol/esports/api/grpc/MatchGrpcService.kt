ca0cc8760ff1252852c9e14084e7750b141c80a3
Thomas McKernan <tmeaglei@gmail.com>
2025-10-23 20:48:08 -0500

    add grpc endpoint for matches for league

--- src/main/kotlin/com/mckernant1/lol/esports/api/grpc/MatchGrpcService.kt
+++ src/main/kotlin/com/mckernant1/lol/esports/api/grpc/MatchGrpcService.kt
@@ -3,19 +3,27 @@ package com.mckernant1.lol.esports.api.grpc
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
+import com.mckernant1.commons.extensions.boolean.falseIfNull
+import com.mckernant1.commons.extensions.time.Instants.isBeforeNow
 import com.mckernant1.lol.esports.api.metrics.PeriodicSubmitCacheStats
+import com.mckernant1.lol.esports.api.svc.LeagueService
 import com.mckernant1.lol.esports.api.svc.MatchService
 import com.mckernant1.lol.esports.api.svc.TournamentService
+import com.mckernant1.lol.esports.api.util.startDateAsDate
 import com.mckernant1.lol.esports.api.util.toGrpc
+import com.mckernant1.lol.matches.GetMatchesForLeagueRequest
+import com.mckernant1.lol.matches.GetMatchesForLeagueResponse
 import com.mckernant1.lol.matches.GetMatchesForTournamentRequest
 import com.mckernant1.lol.matches.GetMatchesForTournamentResponse
 import com.mckernant1.lol.matches.Match
 import com.mckernant1.lol.matches.MatchServiceGrpcKt
 import net.devh.boot.grpc.server.service.GrpcService
 import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @GrpcService
 class MatchGrpcService(
+    private val leagueService: LeagueService,
     private val matchService: MatchService,
     private val tournamentService: TournamentService
 ) : MatchServiceGrpcKt.MatchServiceCoroutineImplBase(), PeriodicSubmitCacheStats {
@@ -29,6 +37,35 @@ class MatchGrpcService(
             .addAllMatches(matchesForTournamentCache[request.tournamentId])
             .build()
 
+    }
+
+    override suspend fun getMatchesForLeague(request: GetMatchesForLeagueRequest): GetMatchesForLeagueResponse {
+        val leagueId = request.leagueId
+        leagueService.assertLeagueExists(leagueId)
+        val limit = if (request.hasLimit()) {
+            request.limit
+        } else {
+            200
+        }
+        val matches = tournamentService.getTournamentsForLeague(leagueId)
+            .filter {
+                it.startDateAsDate()
+                    ?.minus(7, ChronoUnit.DAYS)
+                    ?.isBeforeNow()
+                    .falseIfNull()
+            }.sortedByDescending {
+                it.startDateAsDate()!!
+            }.filter {
+                !leagueId.equals("WCS", ignoreCase = true)
+                        || it.isOfficial.falseIfNull()
+            }.flatMap {
+                matchesForTournamentCache[it.tournamentId]
+            }.take(limit)
+            .toList()
+
+        return GetMatchesForLeagueResponse.newBuilder()
+            .addAllMatches(matches)
+            .build()
     }
 
     private val matchesForTournamentCache: LoadingCache<String, List<Match>> = CacheBuilder.newBuilder()
